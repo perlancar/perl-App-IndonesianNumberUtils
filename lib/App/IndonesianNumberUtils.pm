16ce136d71e762704e0d4816e4c4fb1e4dde1952
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-10 19:02:23 +0700

    Initial commit

---
+++ lib/App/IndonesianNumberUtils.pm
@@ -0,0 +1,18 @@
+package App::IndonesianNumberUtils;
+
+use 5.010001;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLIs related to Indonesian numbers (NIK, NOPPBB, NPWP, etc)
+
+=head1
+
+This distribution contains several CLI's related to Indonesian numbers:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
